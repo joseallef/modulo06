833618254663ebc1e946bd81514c86e4b8a378e7
joseallef <46696111+joseallef@users.noreply.github.com>
2021-04-20 19:59:50 -0300

    final da aula 1 modulo 6

---
+++ projetos/modulo_comum/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Button() {
+  return (
+    <button>
+        Botão criado no [@joseallef/modulo-comum]
+    </button>
+  )
+}
---
+++ projetos/site/pages/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import Button from '@joseallef/modulo-comum/components/Button'
+const Title = styled.h1`
+  color: blue;
+  font-size: 50px;
+`
+
+export default function Home() {
+  return (
+    <Button />
+    <Title>My page so para testes com libs de componentes local</Title>
+  )
+}
